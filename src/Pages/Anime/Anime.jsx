24e56bdc32bdccaa0a21a757ee8e1b2878c30226
carolinamoralese <caromorales1396@gmail.com>
2024-02-21 18:45:03 -0500

    Agregado renderizado de imágenes y sinopsis en la página de Anime. Aplicado estilo con Tailwind CSS para mejorar el diseño.

--- src/Pages/Anime/Anime.jsx
+++ src/Pages/Anime/Anime.jsx
@@ -1,31 +1,39 @@
 import { useEffect, useState } from "react";
 import { petitionAnime } from "../../services/petition";
 
-function Anime(){
+function Anime() {
+  const [animes, setAnimes] = useState(null);
 
-    const [animes, setAnimes] = useState(null);
+  useEffect(() => {
+    petitionAnime().then((responseJson) => {
+      setAnimes(responseJson);
+    });
+  }, []);
 
-    useEffect(() => {
-        petitionAnime().then((responseJson) => {
-            setAnimes(responseJson)
-        })
-
-    },[])
-
-    return (
-        <div>
-            <h1>Lista de anime</h1>
-            {animes ? (
-                <ul>
-                    {animes.map((item) => (
-                        <li key={item.mal_id}>{item.title}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Cargando...</p>
-            )}
-        </div>
-    );
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl text-center font-bold mb-8 p-8">Anime para Todos los Gustos</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+        {animes ? (
+          animes.map((item) => (
+            <div key={item.mal_id} className="bg-gray-200 p-4 rounded-lg cursor-pointer transform transition-transform hover:scale-110">
+              <img
+                src={item.images.webp.image_url}
+                alt={item.title}
+                className="w-full h-64 object-cover rounded-lg"
+              />
+              <div className="mt-2">
+                <h2 className="text-lg font-semibold">{item.title}</h2>
+                <p className="text-sm text-gray-600">{item.synopsis}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Cargando Animes...</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Anime;
--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -4,9 +4,6 @@ import Anime from "../Anime/Anime";
 function App() {
   return (
     <>
-      <h1 className="text-m font-bold underline text-red-400">Hello world!</h1>
-      <div className="bg-green-500 h-10 w-20">Botón</div>
-
       <Anime />
     </>
   );
